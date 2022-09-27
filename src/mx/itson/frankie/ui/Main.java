e74714c1cb412de6ca3003a69486b4da2ea52fd5
Jesús Antonio Ceceña <jantonio.cecena@gmail.com>
2022-09-27 11:16:44 -0700

    Se agregó comentario en Main

--- src/mx/itson/frankie/ui/Main.java
+++ src/mx/itson/frankie/ui/Main.java
@@ -39,6 +39,8 @@ public class Main {
         
         // Agregar pasos
         
+        //Espero que los hayan puesto
+        
         receta.setDificultad(Dificultad.LEGENDARIO);
         
         
